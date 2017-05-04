97ff979da33ec49625ed8bf96a477072e38f600d
John Ryan <john.ryan@cardinalhealth.com>
2017-05-03 20:39:46 -0400

    home page built

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {MessageService} from "../../providers/message-service";
+import {Message} from "../../models/message";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [MessageService]
 })
 export class HomePage {
+  public USER_NAME_CONSTANT = 'John Ryan';
+  public currentMessage: string;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private messageService: MessageService) {
   }
 
+  public send(messageContent: string) {
+    this.buildAndSendMessage(messageContent);
+  }
+
+  private buildAndSendMessage(message: string) {
+    var newMessage = new Message();
+    newMessage.userName = this.USER_NAME_CONSTANT;
+    newMessage.messageContent = message;
+    this.messageService.addMessage(newMessage);
+    this.currentMessage = "";
+  }
 }
